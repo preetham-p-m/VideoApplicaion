f2c774e5fb8d62217998f1b5f2ec50136b557c1f
Preetham P M <Preetham P M@PREETHAM>
2022-10-26 15:25:38 +0530

    Add project files.

---
+++ VSR/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VSR.Models;
+
+namespace VSR.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() { Name="Shrek" };
+            return View(movie);
+        }
+    }
+}
