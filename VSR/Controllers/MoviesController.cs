3bdffadc111dd6a11c8d9096a2d68d3edee9d0e9
preetham-p-m <preetham.p-m@coupa.com>
2022-10-28 13:22:16 +0530

    Added Customer and Movies Controller

--- VSR/Controllers/MoviesController.cs
+++ VSR/Controllers/MoviesController.cs
@@ -1,14 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VSR.Models;
+using VSR.ViewModel.Movies;
 
 namespace VSR.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
+        // Random Movie Page
         public ActionResult Random()
         {
-            Movie movie = new Movie() { Name="Shrek" };
-            return View(movie);
+            var movie = new Movie{ Name="F1" };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="C1" },
+                new Customer{Name="C2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers,
+            };
+            return View(viewModel);
         }
+
     }
 }
---
+++ VSR/ViewModel/Movies/RandomMovieViewModel.cs
@@ -0,0 +1,10 @@
+﻿using VSR.Models;
+
+namespace VSR.ViewModel.Movies
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
