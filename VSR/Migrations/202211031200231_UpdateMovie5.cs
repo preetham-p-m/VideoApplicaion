b0fb2b76e2b9cfb7316678dd1f93f0d1d84a60cf
Preetham P M <preethampmsmg@gmail.com>
2022-11-05 12:34:57 +0530

    Update Migration on Movies Table

--- VSR/Migrations/202211031200231_UpdateMovie5.cs
+++ VSR/Migrations/202211031200231_UpdateMovie5.cs
@@ -10,19 +10,10 @@
             DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
             DropIndex("dbo.Movies", new[] { "GenreId" });
             DropColumn("dbo.Movies", "GenreId");
-            DropTable("dbo.Genres");
         }
         
         public override void Down()
         {
-            CreateTable(
-                "dbo.Genres",
-                c => new
-                    {
-                        Id = c.Byte(nullable: false),
-                        Name = c.String(nullable: false, maxLength: 255),
-                    })
-                .PrimaryKey(t => t.Id);
             
             AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
             CreateIndex("dbo.Movies", "GenreId");
